e05305e168e73a7b7c4a37a88411a49259fae377
Joakim Hove <joakim.hove@gmail.com>
2017-11-23 13:08:55 +0100

    output

--- fmu_storage/wsgi.py
+++ fmu_storage/wsgi.py
@@ -9,14 +9,18 @@ https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 import json
 import os
 import os.path
+import sys
 
 from django.core.wsgi import get_wsgi_application
 settings_file = os.path.join( os.path.dirname(__file__), "fmu_settings.json")
+sys.stderr.write("Loading settings from:%s" % settings_fileimport os.path
+)
 if os.path.isfile(settings_file):
     with open(settings_file) as fileH:
         settings = json.load(fileH)
 
     for key,value in settings.iteritems():
         os.environ[key] = value
+        sys.stderr.write("Setting %s=%s\n" % (key, value))
 
 application = get_wsgi_application()
