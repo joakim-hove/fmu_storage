981ff42614e99d65a2a6d98ad400092f0526b7c8
Joakim Hove <joakim.hove@gmail.com>
2017-11-23 13:19:45 +0100

    Changes

--- ensemble/admin.py
+++ ensemble/admin.py
@@ -3,4 +3,11 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
-# Register your models here.
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.contrib import admin
+from simulation.models import *
+
+admin.site.register(Ensemble)
+admin.site.register(Realisation)
--- fmu_storage/wsgi.py
+++ fmu_storage/wsgi.py
@@ -11,10 +11,8 @@ import os
 import os.path
 import sys
 
-from django.core.wsgi import get_wsgi_application
 settings_file = os.path.join( os.path.dirname(__file__), "fmu_settings.json")
-sys.stderr.write("Loading settings from:%s" % settings_fileimport os.path
-)
+sys.stderr.write("Loading settings from:%s\n" % settings_file) 
 if os.path.isfile(settings_file):
     with open(settings_file) as fileH:
         settings = json.load(fileH)
@@ -23,4 +21,6 @@ if os.path.isfile(settings_file):
         os.environ[key] = value
         sys.stderr.write("Setting %s=%s\n" % (key, value))
 
+
+from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
