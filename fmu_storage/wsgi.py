92f7c47a167e639c2679d3e88537cd2b28080a3a
Joakim Hove <joakim.hove@gmail.com>
2017-11-23 12:37:03 +0100

    Changes.

--- fmu_storage/wsgi.py
+++ fmu_storage/wsgi.py
@@ -8,9 +8,10 @@ https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 import json
 import os
+import os.path
 
 from django.core.wsgi import get_wsgi_application
-settings_file = "fmu_settings.json"
+settings_file = os.path.join( os.path.dirname(__file__), "fmu_settings.json")
 if os.path.isfile(settings_file):
     with open(settings_file) as fileH:
         settings = json.load(fileH)
