fe8d4eed5e00a212a9b60137823394e7726ee3ec
Joakim Hove <joakim.hove@gmail.com>
2017-12-07 13:35:49 +0100

    Changes

--- simulation/api/urls.py
+++ simulation/api/urls.py
@@ -5,6 +5,6 @@ from .views import *
 urlpatterns = [
     url(r'^(?P<id>[0-9]+)/summary/data/' , SummaryData.as_view() , name = "api.simulation.summary.data"),
     url(r'^(?P<id>[0-9]+)/summary/keys/$' , SummaryKeys.as_view() , name = "api.simulation.summary.keys"),
-    url(r'^(?P<id>[0-9]+)/parameters/data/$' , Parameters.as_view() , name = "api.simulation.parameters"),
+    url(r'^(?P<id>[0-9]+)/parameters/$' , Parameters.as_view() , name = "api.simulation.parameters"),
     url(r'^upload/$' , Upload.as_view() , name = "api.simulation.upload")
 ]
