19ec7924cdf4194371e1976a6e203fa6c07806d3
Joakim Hove <joakim.hove@gmail.com>
2017-12-08 17:01:03 +0100

    Changes

--- fmu_storage/api/views.py
+++ fmu_storage/api/views.py
@@ -18,7 +18,7 @@ class ApiUrls(View):
     def get(self, request):
         url_list = [ make_url("api.ensemble.get_or_create", {}, "Doc"),
                      make_url("api.ensemble.add_simulation", {"ens_id" :  9999}, "Doc"),
-                     make_url("api.ensemble.simulation.summary.data", {"ens_id" : 9999}, "Doc"),
+                     make_url("api.ensemble.simulations.summary.data", {"ens_id" : 9999}, "Doc"),
                      make_url("api.ensemble.info", {"ens_id" : 9999}, "Doc"),
                      make_url("api.simulation.summary.data", {"id" : 9999}, "Doc"),
                      make_url("api.simulation.parameters", {"id" : 9999}, "Doc")]
